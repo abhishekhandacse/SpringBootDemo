896b809eeaf935bc5622f1f4edb91904171de5e7
Abhishek Handa <abhishekhandacse@gmail.com>
2020-04-10 17:40:29 +0530

    Used GetMapping Annotation

--- src/main/java/com/web/restapi/HelloWorldController.java
+++ src/main/java/com/web/restapi/HelloWorldController.java
@@ -1,4 +1,23 @@
 package com.web.restapi;
 
-public class HelloWorld {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+    /*
+    * Step 1 GET
+    * Step 2 URI
+    * Step 3 method - " Hello World"
+    *
+    */
+//    @RequestMapping(method= RequestMethod.GET,path="/hello-world")
+    @GetMapping(path="hello-world")
+    public String helloWorld(){
+        return " Hello World";
+    }
+
 }
--- src/main/java/com/web/restapi/RestapiApplication.java
+++ src/main/java/com/web/restapi/RestapiApplication.java
@@ -8,7 +8,6 @@ public class RestapiApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
