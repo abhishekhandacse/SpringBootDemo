27a10e7f40d3c6688b44d3447a6f73f7cc4bd582
Abhishek Handa <abhishekhandacse@gmail.com>
2020-04-14 20:46:22 +0530

    Added Locale for France and Added Jackson dependency for XML support

--- src/main/java/com/web/restapi/RestapiApplication.java
+++ src/main/java/com/web/restapi/RestapiApplication.java
@@ -1,7 +1,15 @@
 package com.web.restapi;
 
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestapiApplication {
@@ -10,4 +18,10 @@ public class RestapiApplication {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver=new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
--- src/main/java/com/web/restapi/helloworld/HelloWorldController.java
+++ src/main/java/com/web/restapi/helloworld/HelloWorldController.java
@@ -1,10 +1,23 @@
 package com.web.restapi.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationlized(){
+        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
+    }
     /*
     * Step 1 GET
     * Step 2 URI
