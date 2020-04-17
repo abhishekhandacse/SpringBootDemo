19b7d8bc4154d877fb5689229f2915f6fd6f5a99
Abhishek Handa <abhishekhandacse@gmail.com>
2020-04-18 01:05:04 +0530

    Created entity called user

--- src/main/java/com/web/restapi/user/User.java
+++ src/main/java/com/web/restapi/user/User.java
@@ -5,14 +5,18 @@ package com.web.restapi.user;
 import com.sun.istack.NotNull;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2,message = "Name should have atleast 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1,sysdate(),'Abhishek Handa');
+insert into user values(2,sysdate(),'Vishu Handa');
+insert into user values(3,sysdate(),'Rajesh kumar');
